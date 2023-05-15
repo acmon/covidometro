81349aaf817374608bf9a24483d6e02df77ca74e
acmon <acmon.soares@gmail.com>
2023-05-15 20:51:56 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <nav class="nav">
+      <div>
+        <img src="./assets/images/logo.png" alt="logo da página" />
+      </div>
+    </nav>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+.nav {
+  background-color: #fff;
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media (min-width: 768px) {
+  .nav {
+    padding-left: 5rem;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
